4c538d5c5a805227bb0a096bddcdd6a687e1d024
DannyJoe1994 <1916931393@qq.com>
2019-04-19 21:19:04 +0800

    微調

--- src/main/java/com/joe/business/commodity/controller/CommodityController.java
+++ src/main/java/com/joe/business/commodity/controller/CommodityController.java
@@ -3,15 +3,18 @@ package com.joe.business.commodity.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.joe.api.po.Commodity;
+import com.joe.common.ApiParam;
 import com.joe.common.base.BaseController;
 import com.joe.business.commodity.service.CommodityWebService;
 import com.joe.business.commodity.vo.CommodityVo;
 import com.joe.util.mvc.ResponseEntity;
 import com.joe.util.mvc.ResponsePageEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,29 +24,24 @@ import java.util.List;
 /**
  * 商品管理
  */
+@Slf4j
 @RestController
 @RequestMapping("/commodity")
-public class CommodityController extends BaseController {
-
-    private static final Logger logger = LoggerFactory.getLogger(CommodityController.class);
+public class CommodityController {
 
     @Autowired
     private CommodityWebService commodityWebService;
 
 
     @RequestMapping("/addCommodity")
-    public Object addCommodity(HttpServletRequest request) {
+    public Object addCommodity(@RequestBody ApiParam apiParam) {
 
-        String requestParam = getRequestParam(request);
-        if (StringUtils.isBlank(requestParam)) {
-            return ResponseEntity.getFailEntity("参数错误");
-        }
-
+        String requestParam = apiParam.getBody();
         CommodityVo commodityVo = JSON.parseObject(requestParam, CommodityVo.class);
 
-        logger.info("request add commodity, commodity name is {}.", commodityVo.getpName());
+        log.info("request add commodity, commodity name is {}.", commodityVo.getpName());
         int commodityId = commodityWebService.addCommodity(commodityVo);
-        logger.info("add commodity success, commodity id is {}.", commodityId);
+        log.info("add commodity success, commodity id is {}.", commodityId);
 
         return ResponseEntity.getSuccessEntity("新增商品成功", commodityId);
     }
@@ -51,27 +49,21 @@ public class CommodityController extends BaseController {
 
     /**
      * 根据类目id查询商品集合
-     *
-     * @param request
-     * @return
      */
     @RequestMapping("/getCommodityListByItemId")
-    public Object getCommodityListByItemId(HttpServletRequest request) {
+    public Object getCommodityListByItemId(@RequestBody ApiParam apiParam) {
 
-        String requestParam = getRequestParam(request);
-        if (StringUtils.isBlank(requestParam)) {
-            return ResponseEntity.getFailEntity("参数有误");
-        }
+        String requestParam = apiParam.getBody();
 
         JSONObject jsonObject = JSON.parseObject(requestParam);
         Integer itemId = Integer.valueOf(jsonObject.get("itemId").toString());
         Integer pageNo = Integer.valueOf(jsonObject.get("pageNo").toString());
         Integer pageSize = Integer.valueOf(jsonObject.get("pageSize").toString());
-        logger.info("request commodity list, param  item {}, pageNo {}, pagesSize{}.", itemId, pageNo, pageSize);
+        log.info("request commodity list, param  item {}, pageNo {}, pagesSize{}.", itemId, pageNo, pageSize);
 
         int total = commodityWebService.queryCommodityCountByItemId(itemId);
         List<Commodity> commodityList = commodityWebService.queryCommodityByItemId(itemId, pageNo, pageSize);
-        logger.info("request commodity list success.");
+        log.info("request commodity list success.");
 
         ResponsePageEntity pageEntity = new ResponsePageEntity();
         pageEntity.setTotal(total);
@@ -89,9 +81,9 @@ public class CommodityController extends BaseController {
     @RequestMapping("/getRecommendCommodity")
     public Object getRecommendCommodity() {
 
-        logger.info("request recommend commodity list");
+        log.info("request recommend commodity list");
         List<Commodity> commodities = commodityWebService.queryRecommendCommodity();
-        logger.info("request recommend commodity list success, total {}", commodities.size());
+        log.info("request recommend commodity list success, total {}", commodities.size());
 
         return ResponseEntity.getSuccessEntity("查询推荐商品成功", commodities);
     }
@@ -100,24 +92,19 @@ public class CommodityController extends BaseController {
     /**
      * 切换商品推荐/不推荐
      *
-     * @param request
-     * @return
      */
     @RequestMapping("/switchCommodityRecommend")
-    public Object switchCommodityRecommend(HttpServletRequest request) {
+    public Object switchCommodityRecommend(@RequestBody ApiParam apiParam) {
 
-        String requestParam = getRequestParam(request);
-        if (StringUtils.isBlank(requestParam)) {
-            return ResponseEntity.getFailEntity("param error");
-        }
+        String requestParam = apiParam.getBody();
 
         JSONObject jsonObject = JSON.parseObject(requestParam);
         Integer commodityId = Integer.valueOf(jsonObject.get("commodityId").toString());
-        logger.info("request change commodity recommend status, commodity id is {}.", commodityId);
+        log.info("request change commodity recommend status, commodity id is {}.", commodityId);
 
         int executeNum = commodityWebService.updateRecommendStatus(commodityId);
 
-        logger.info("change commodity recommend status success.");
+        log.info("change commodity recommend status success.");
         return ResponseEntity.getSuccessEntity("修改推荐状态成功", executeNum);
 
     }
@@ -126,20 +113,15 @@ public class CommodityController extends BaseController {
     /**
      * 删除商品
      *
-     * @param request
-     * @return
      */
     @RequestMapping("/removeCommodity")
-    public Object removeCommodity(HttpServletRequest request) {
+    public Object removeCommodity(@RequestBody ApiParam apiParam) {
 
-        String requestParam = getRequestParam(request);
-        if (StringUtils.isBlank(requestParam)) {
-            return ResponseEntity.getFailEntity("参数错误");
-        }
+        String requestParam = apiParam.getBody();
 
         JSONObject jsonObject = JSON.parseObject(requestParam);
         Integer commodityId = Integer.valueOf(jsonObject.get("commodityId").toString());
-        logger.info("request remove commodity, commodity id is {}.", commodityId);
+        log.info("request remove commodity, commodity id is {}.", commodityId);
 
         int executeNum = commodityWebService.removeCommodity(commodityId);
         return ResponseEntity.getSuccessEntity("删除商品成功", executeNum);
@@ -149,28 +131,23 @@ public class CommodityController extends BaseController {
     /**
      * 修改商品
      *
-     * @param request
-     * @return
      */
     @RequestMapping("/updateCommodity")
-    public Object updateCommodity(HttpServletRequest request) {
-        String requestParam = getRequestParam(request);
-        if (StringUtils.isBlank(requestParam)) {
-            return ResponseEntity.getFailEntity("参数错误");
-        }
+    public Object updateCommodity(@RequestBody ApiParam apiParam) {
+
+        String requestParam = apiParam.getBody();
         CommodityVo commodityVo = JSON.parseObject(requestParam, CommodityVo.class);
 
-        logger.info("request modify commodity, commodity id is {}", commodityVo.getpId());
+        log.info("request modify commodity, commodity id is {}", commodityVo.getpId());
         int executeNum = commodityWebService.updateCommodity(commodityVo);
 
         if (executeNum == 0) {
-            logger.info("modify commodity fail");
+            log.info("modify commodity fail");
             return ResponseEntity.getFailEntity("修改商品失败");
         }
 
-        logger.info("modify commodity success");
+        log.info("modify commodity success");
         return ResponseEntity.getSuccessEntity("商品修改成功", executeNum);
-
     }
 
 
---
+++ src/main/java/com/joe/common/ApiParam.java
@@ -0,0 +1,13 @@
+package com.joe.common;
+
+import lombok.Data;
+
+@Data
+public class ApiParam {
+
+    private String timeStr;
+
+    private String signature;
+
+    private String body;
+}
