268506bdf14316289ef0b60068d53ebb2d877af8
zhuhongjun <zhuhongjun123>
2019-05-24 11:11:58 +0800

    feat 微信支付回调接口 Joe

--- src/main/java/com/joe/common/LogAspect.java
+++ src/main/java/com/joe/common/LogAspect.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class LogAspect {
 
 
-    @Pointcut("execution(* com.joe.controller.CommodityController..*(..))")
+    @Pointcut("execution(* com.joe.controller.*..*(..))")
     public void controllerPoint() {
         //controller切入点
     }
